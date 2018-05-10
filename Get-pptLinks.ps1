8d44161cde10e67d622a472cc1d37430e82c313e
ZacharyOHanlon <31595592+ZacharyOHanlon@users.noreply.github.com>
2018-05-10 18:14:17 -0400

    Add files via upload
    Initial upload

---
+++ Get-pptLinks.ps1
@@ -0,0 +1,88 @@
+﻿Function IndexLinks {
+Param ($path)
+
+$folder = $env:TEMP + "\" +  [System.IO.Path]::GetFileNameWithoutExtension($path)
+$zippath = $env:TEMP + "\" +  [System.IO.Path]::GetFileNameWithoutExtension($path) + '.zip'
+$Filename = [System.IO.Path]::GetFileName($path)
+Write-Host 'Checking' $Filename 'for links'
+Copy-Item $path -Destination $zippath
+Expand-Archive $zippath -DestinationPath $folder -Force
+
+
+$slides = get-childitem $folder -filter *.xml.rels -recurse
+$xml = New-Object -TypeName XML
+$Links = @()
+ForEach ($slide in $slides) {
+    
+    $xml.load($slide.fullname)
+    #Write-Host $slide.FullName
+    #$xml.Relationships.ChildNodes | format-list -property $_.Target
+    $LinkNodes = $xml.Relationships.ChildNodes | where {$_.Target -like "file*"} 
+    foreach ($LinkNode in $LinkNodes){
+        $SlideNumber = $slide.Name -replace '.xml.rels',''
+        $HyperLink = $LinkNode.Target -replace 'File:///','' -replace '%20',' '
+        $Link = new-object psobject -prop @{Filename=$Filename;Path=$path; Hyperlink_Type='Hyperlink'; Link=$Hyperlink; Slide_Number=$SlideNumber; New_link=''; ID=$LinkNode.Id}
+        $Links += $Link   
+    }
+    
+    #clear
+    #$xml.Save($slide.FullName)
+    
+} #End of main loop
+$Links = $Links | Sort-Object -Property Slide_Number
+
+Remove-Item $zippath
+Remove-Item $folder -Recurse
+
+Return $Links
+}
+
+Function FixLinks {
+    param ($Hyperlink)
+    if ($hyperlink.IndexOf('#') -gt 5){
+        Write-host 'powerpoint link contains bookmark'
+        $index = $hyperlink.IndexOf('#')
+        $Hyperlink = $Hyperlink.Substring(0, $index)
+    }
+    if ([System.IO.Path]::GetExtension($hyperlink) -like ".lnk"){$Hyperlink = $Hyperlink -replace '.lnk',''}
+    $SearchFolder = $Directory.indexofany("\",(([system.io.path]::GetPathRoot($directory).length) + 1 )) 
+    $SearchFolder = $Directory.substring(0,$SearchFolder)
+    $FileName = [System.IO.Path]::GetFileName($Hyperlink)
+    if (($hyperlink -like "\\srv*") -and ($hyperlink -notlike $directory.Substring(0,9))){
+        $NewHyperlink = (Get-ChildItem -path $SearchFolder -Filter $FileName -Exclude "*.lnk" -Recurse).FullName | Select-Object -First 1
+        $SearchFolder = [system.io.path]::GetPathRoot($Directory)
+        if ($NewHyperLink -like ''){$NewHyperlink = (Get-ChildItem -path $SearchFolder -Filter $FileName -Exclude "*.lnk" -Recurse).FullName | Select-Object -First 1}
+        Return $NewHyperlink;
+        break
+       }
+    If (Test-Path $Hyperlink){
+        Return 'Functional Link'
+    }
+    Else {
+        #$NewHyperlink = (Get-ChildItem -path $Directory -Filter $FileName -Recurse).FullName | Select-Object -First 1
+        $NewHyperlink = (Get-ChildItem -path $SearchFolder -Filter $FileName -Exclude "*.lnk" -Recurse).FullName | Select-Object -First 1
+        $SearchFolder = [system.io.path]::GetPathRoot($Directory)
+        if ($NewHyperLink -like ''){$NewHyperlink = (Get-ChildItem -path $SearchFolder -Filter $FileName -Exclude "*.lnk" -Recurse).FullName | Select-Object -First 1}
+        Return $NewHyperlink
+    }
+    Return ''
+}
+
+$OutputPath = $env:USERPROFILE + '\Desktop\Powerpoint_log.csv'
+$links = @()
+clear
+$Directory = Read-Host "Where are the powerpoints?"
+clear
+Write-Host "Finding Powerpoints"
+$Powerpoints = Get-ChildItem -Path $Directory -Filter *.ppt* -recurse -Exclude '*.ppt','*.pps','*.lnk'
+Write-Host 'Powerpoints found, checking powerpoints for Links'
+ForEach ($Powerpoint in $powerpoints){
+    $links += IndexLinks $Powerpoint.FullName    
+}
+Write-Host 'Links found, Checking if links are working'
+ForEach ($link in $links){
+    Write-Host "Testing links in" $link.Filename
+    $Link.New_Link = FixLinks $link.Link
+}
+$Links | select-object Filename, Slide_Number, Link, New_link, Path, ID | Export-csv -path $OutputPath -encoding ascii -NoTypeInformation -Force
+Write-host 'Finished'
---
+++ Set-pptLinks.ps1
@@ -0,0 +1,50 @@
+﻿Function ReplaceLinks {
+Param ($Links)
+$path = $Links[0].Path
+$folder = $env:TEMP + "\" +  [System.IO.Path]::GetFileNameWithoutExtension($path)
+$zippath = $env:TEMP + "\" +  [System.IO.Path]::GetFileNameWithoutExtension($path) + '.zip'
+$Filename = [System.IO.Path]::GetFileName($path)
+Write-Host 'Replacing links in ' $Filename
+Copy-Item $path -Destination $zippath
+Expand-Archive $zippath -DestinationPath $folder -Force
+
+    ForEach ($link in $links){
+    $Link.Link = 'file:///' + $link.Link
+    $Link.New_Link = 'file:///' + $link.New_Link
+    $Link.Link = $link.Link -replace ' ','%20'
+    $Link.New_link = $link.New_Link -replace ' ','%20'
+    $filter = $Link.Slide_Number + '.xml.rels'
+    $slide = get-childitem $folder -filter $filter -recurse | Select-Object -First 1
+    $xml = New-Object -TypeName XML
+    $xml.Load($slide.FullName)
+    
+
+    $LinkNode = $xml.SelectNodes("//*[@Id]") | Where-Object {$_.Id -like $Link.Id}
+    $LinkNode
+    if ($linkNode.Target -like $Link.link){$LinkNode.Target = $link.New_Link}
+    $xml.save($slide.FullName)
+    }
+$Extension = [System.IO.Path]::GetExtension($path)
+$Fixed = '-fixed' + $Extension
+$OutputPath = $path -replace $Extension,$Fixed
+$contents = $folder + '\*'
+Compress-Archive $contents -DestinationPath $zippath -Force
+Copy-Item $zippath -Destination $OutputPath
+Remove-Item $zippath
+Remove-Item $folder -Recurse
+
+
+}
+
+
+
+clear
+$LinkPath = $env:USERPROFILE + '\Desktop\Powerpoint_log.csv'
+$Logs = import-csv $LinkPath
+$logs = $logs | Sort-Object -Property Path -Descending | Where-Object {(($_.New_Link -ne 'Functional Link') -and ($_.New_Link -ne ""))}
+$Files = $logs | sort-object -Property Path -Unique
+ForEach ($File in $Files){
+    $links = $logs | Where-Object {$_.path -like $file.path}
+   ReplaceLinks $links
+}
+Write-Host "Finished replacing links"
